6e1586364386afc214da1a1e318ff6936a57f698
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-05-18 12:13:32 +0300

    learned about concatenation

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('HELLO WORLD.');
+console.error('This is an error.');
+console.warn('This is a warning.');
+
+//There are three types of variables: var, let, cost
+//With the let variable you can reassign values but with the const variable you cannot reassign values.
+
+/*
+There are different types of data types in JS ,they include:strings, numbers, symbols, booleans, null, 
+*/
+
+/* this is what concatenate means */
+const name ='John';
+const age = 30;
+
+console.log('My name is ' + name + ' and I am ' + age + ' years old');
+
+//You can also use the template string
+
+console.log(`My name is ${name} and I am ${age} years old.`)
